c929fa859642713182d5bd8552c942820339a68f
Ann Qian <ann.qian07@hotmail.com>
2020-10-26 03:00:58 +1100

    firsttt owo

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content === "!owo") {
+    message.channel.send("UwU");
+  }
+});
+
+client.login(config.token);
